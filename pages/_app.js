82a382da06f749144dd19c11d33f9506e858b013
Ignacio Tomás <itomas@trendico.es>
2022-10-06 17:50:04 +0200

    added google analitys

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,49 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Layout from '../components/layout.js';
+import Script from "next/script";
 import { appWithTranslation } from 'next-i18next';
 import '../styles/globals.scss';
 import '../styles/fontawesome/all.css';
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-49EE3B2GDW`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-49EE3B2GDW', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
